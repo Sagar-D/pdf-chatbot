396c2b03311f22bd28873ea3efa06dc233e0d344
Sagar D <sagard.cmagalur@gmail.com>
2025-12-18 18:28:57 +0530

    Improve UI to handle errors; disable input fields while processing request

--- pdf_chatbot/chat/chat_handler.py
+++ pdf_chatbot/chat/chat_handler.py
@@ -1,36 +1,34 @@
 from pdf_chatbot.rag.rag_agent import RAGAgent, RAGAgentState
+from pdf_chatbot.documents.document_processor import DocumentProcessor
 from langchain.messages import AIMessage, HumanMessage
 from typing import Union
 import gradio as gr
+
 
 class ChatHandler:
 
     def __init__(self):
         self.agent = RAGAgent()
-        self.agent_state: RAGAgentState = {
-            "input": "",
-            "messages": [],
-            "files": [],
-        }
+        self.agent_state: RAGAgentState = {"input": "", "messages": []}
 
-    def chat(self, query: str, files: list[str], llm_platform: str):
+    def chat(self, query: str, files: list[bytes], llm_platform: str):
 
         query = query or ""
         files = files or []
         llm_platform = llm_platform or self.agent_state.get("llm_platform") or "ollama"
 
         if query.strip() == "":
-            raise gr.Error("Please enter a User query in the input box")
-        if len(self.agent_state["files"]) == 0 and len(files) == 0:
-            raise gr.Error(
-                "Value Error: No PDF file uploaded. Please attach a PDF file"
-            )
+            gr.Warning("Please enter a User query in the input box")
+            return self._generate_gradio_chat(self.agent_state["messages"])
+        if len(files) == 0:
+            gr.Warning("No PDF file uploaded. Please attach a PDF file")
+            return self._generate_gradio_chat(self.agent_state["messages"])
 
         if len(files) > 0:
-            self.agent_state["files"] = files
+            self._ingest_knowledge(files)
+
         self.agent_state["input"] = query
         self.agent_state["llm_platform"] = llm_platform
-
         self.agent_state = self.agent.invoke(self.agent_state)
         return self._generate_gradio_chat(self.agent_state["messages"])
 
@@ -48,3 +46,6 @@ class ChatHandler:
                 continue
             history.append({"role": role, "content": message.content})
         return history
+
+    def _ingest_knowledge(self, files:list[bytes]):
+        DocumentProcessor().store_docs_to_db(files)
--- pdf_chatbot/chat/gradio_chat_ui.py
+++ pdf_chatbot/chat/gradio_chat_ui.py
@@ -2,10 +2,34 @@ import gradio as gr
 import pdf_chatbot.config as config
 
 
+def _disable_inputs(msg: str):
+    return (
+        msg,
+        gr.update(interactive=False),
+        gr.update(
+            value="",
+            placeholder="Processing your request… please wait",
+            interactive=False,
+        ),
+        gr.update(interactive=False),
+        gr.update(interactive=False),
+    )
+
+
+def _enable_inputs():
+    return (
+        gr.update(interactive=True),
+        gr.update(value="", placeholder="Type your query here...", interactive=True),
+        gr.update(interactive=True),
+        gr.update(interactive=True),
+    )
+
+
 def create_gradio_chat_interface(fn):
 
     with gr.Blocks() as app:
         chatbot = gr.Chatbot(height=450)
+        msg_state = gr.State()
 
         with gr.Row():
             llm_platform = gr.Dropdown(
@@ -16,11 +40,23 @@ def create_gradio_chat_interface(fn):
             )
             msg = gr.Textbox(placeholder="Type your query here...", scale=8)
             files = gr.File(
-                file_count="multiple", type="filepath", file_types=[".pdf"], height=80
+                file_count="multiple", type="binary", file_types=[".pdf"], height=80
             )
             send = gr.Button("Send", scale=1)
 
-        send.click(fn=fn, inputs=[msg, files, llm_platform], outputs=[chatbot])
-        msg.submit(fn=fn, inputs=[msg, files, llm_platform], outputs=[chatbot])
-    
+        send.click(
+            fn=_disable_inputs,
+            inputs=[msg],
+            outputs=[msg_state, llm_platform, msg, files, send],
+        ).then(fn=fn, inputs=[msg_state, files, llm_platform], outputs=[chatbot]).then(
+            fn=_enable_inputs, inputs=[], outputs=[llm_platform, msg, files, send]
+        )
+        msg.submit(
+            fn=_disable_inputs,
+            inputs=[msg],
+            outputs=[msg_state, llm_platform, msg, files, send],
+        ).then(fn=fn, inputs=[msg_state, files, llm_platform], outputs=[chatbot]).then(
+            fn=_enable_inputs, inputs=[], outputs=[llm_platform, msg, files, send]
+        )
+
     return app
